f8926b2d6c52c8ecfe270b6b6d9448b2cdad7570
LindsayMiller <lmm566@gmail.com>
2018-10-30 21:14:08 -0400

    Adds static input field

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import './Search.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faGhost)
 
 class Search extends Component {
   render() {
     return (
       <div className="search">
-        This is the search component. 
+        <div className="search-input">
+        <label>
+          <FontAwesomeIcon icon="ghost" className="search-icon" />
+          <input type="text" placeholder="Search for a place"/>
+          {/* TODO: Add value this.props.query and onclick event */}
+        </label>
+          
+        </div>
       </div>
     )
   }
 }
 
 export default Search;
+
