dd62fd778337686b71ed5c6011cd3e5c18df29be
LindsayMiller <lmm566@gmail.com>
2018-11-03 11:40:29 -0400

    Updates

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery'; 
 import InfoWindowAdopt from '../InfoWindowAdopt';
-import { axios, jsonpAdapter } from 'axios-jsonp';
 
  class InfoWindow extends Component {
 
@@ -19,39 +18,59 @@ import { axios, jsonpAdapter } from 'axios-jsonp';
         console.log("getting data")
         const pfApiKey = process.env.REACT_APP_PETFINDER_API_KEY;
 
-    $.ajax({
-        url: 'http://api.petfinder.com/pet.getRandom?',
-        jsonp: "callback",
-        dataType: 'jsonp',
-        data: {
-            key: pfApiKey,
-            animal: 'cat',
-            output: 'basic',
-            format: 'json',
-        },
-        cache: false,
-        success: function (data){
-           this.setState = {data};
-            console.log(data);
-            // var pfCatName= data.petfinder.pet.name.$t;
-            // var id = data.petfinder.pet.id.$t;
-            // var linkToPfCat = `https://www.petfinder.com/petdetail/${id}`;
-            // console.log(id,linkToPfCat);
-       }
-        .bind(this),
-        error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this),
-      });
+        // fetchJsonp(`http://api.petfinder.com/pet.getRandom?key=${pfApiKey}&format=json`,
+        //     {
+        //         jsonpCallback: 'cb',
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             // "X-Content-Type-Options": nosniff,
+        //             // "Content-Type": "application/x-www-form-urlencoded",
+        //         } 
+        //     })
+        // .then(function(response) {
+        //     console.log(response.json());
+        //   return response.json()
+        // }).then(function(json) {
+        //   console.log('parsed json', json)
+        // }).catch(function(ex) {
+        //   console.log('parsing failed', ex)
+        // })
+
+        $.ajax({
+            url: 'http://api.petfinder.com/pet.getRandom?',
+            jsonp: "callback",
+            dataType: 'jsonp',
+            data: {
+                key: pfApiKey,
+                animal: 'cat',
+                output: 'basic',
+                format: 'json',
+            },
+            cache: false,
+            success: function (data){
+                console.log(data);
+                const array = data.response;
+                console.log(array);
+                // var pfCatName= data.petfinder.pet.name.$t;
+                // var id = data.petfinder.pet.id.$t;
+                // var linkToPfCat = `https://www.petfinder.com/petdetail/${id}`;
+                // console.log(id,linkToPfCat);
+        }
+            .bind(this),
+            error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+            }.bind(this),
+        });
     }
 
     // https://jaketrent.com/post/react-componentdidmount-not-called-server-render/
+  
      
     
 
   render() {
 
-    console.log(this.state.data);
+    // console.log(this.state.data);
 
    return (
     <li className="infowindow" id={this.props.id}>
